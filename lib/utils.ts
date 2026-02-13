18d907d516b6afd29bbefa39b4a723d179f32416
NourAlHoudaRifaieh <nouralhoudarifaieh@gmail.com>
2026-02-13 15:21:29 +0200

    feat: add utility functions

---
+++ lib/utils.ts
@@ -0,0 +1,16 @@
+export function cn(...classes: (string | undefined | null | false)[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+export function formatDate(date: Date): string {
+  return new Intl.DateTimeFormat('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    year: 'numeric'
+  }).format(date);
+}
+
+export function generateId(): string {
+  return Math.random().toString(36).substring(2, 9);
+}
