201333d4fc3fce5ed9fe32e9f97453413746dc2a
NourAlHoudaRifaieh <nouralhoudarifaieh@gmail.com>
2026-02-15 17:55:23 +0200

    feat: Fixed the responsive of the dashboard

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,32 @@
+'use client';
+
+import { useState } from 'react';
+import { Sidebar } from '@/components/layout/Sidebar';
+import { Header } from '@/components/layout/Header';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen bg-gray-50">
+
+      {/* Sidebar */}
+      <Sidebar isOpen={isOpen} setIsOpen={setIsOpen} />
+
+      {/* Main Content */}
+      <div className="flex-1 flex flex-col">
+
+        <Header setIsOpen={setIsOpen} />
+
+        <main className="flex-1 overflow-y-auto">
+          {children}
+        </main>
+
+      </div>
+    </div>
+  );
+}
